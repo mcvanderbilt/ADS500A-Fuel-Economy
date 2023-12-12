60731fe1247f86064398276a36f499b996985ead
Matthew C. Vanderbilt <mvanderbilt@ucsd.edu>
2023-12-11 18:20:11 -0800

    Code Transfer
    Creating project in new GitHub

---
+++ code.R
@@ -0,0 +1,25 @@
+# Exploratory data analysis of fuel economy data from the EPA
+
+# Install Necessary Packages
+install.packages("ggplot2")     #Enables visualization of data
+install.packages("ggcorrplot")  #Creates correlation matrix heatmap
+install.packages("car")         #Used for calculation of Variance Inflation Factor (VIF)
+
+# Load Necessary Packages
+library(ggplot2)        #Enables visualization of data
+library(ggcorrplot)     #Creates correlation matrix heatmap
+library(car)            #Used for calculation of Variance Inflation Factor (VIF)
+
+# Load Vehicles Data
+file_vehicles <- "data/vehicles.csv"
+data_vehicles <- read.csv(file_vehicles)
+head(data_vehicles)
+str(data_vehicles)
+summary(data_vehicles)
+
+# Load Emissions Data
+file_emissions <- "data/emissions.csv"
+data_emissions <- read.csv(file_emissions)
+head(data_emissions)
+str(data_emissions)
+summary(data_vehicles)

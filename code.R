c9d69fd6dd67da7a97a95d6fe30b65a17cd9c8e0
Matthew C. Vanderbilt <mvanderbilt@ucsd.edu>
2023-12-11 21:58:37 -0800

    Working through analysis

--- code.R
+++ code.R
@@ -27,7 +27,6 @@ file_vehicles <- "data/vehicles.csv"
 data_vehicles <- read.csv(file_vehicles)
 count_vehicles <- nrow(data_vehicles)
 head(data_vehicles)
-head_vehicles <- head(data_vehicles)
 str(data_vehicles)
 summary(data_vehicles)
 
@@ -39,19 +38,40 @@ head(data_emissions)
 str(data_emissions)
 summary(data_vehicles)
 
-# Create Dataset of Interest
-data_study <- data_vehicles[, c("barrels08","co2TailpipeGpm","comb08","cylinders","displ","fuelType","make","model","trany")]
+# Add calculated field(s)
+data_vehicles$volume <- data_vehicles$hlv + data_vehicles$hpv + data_vehicles$lv2 + data_vehicles$lv4 + data_vehicles$pv2 + data_vehicles$pv4
+data_vehicles$transmission <- substr(data_vehicles$trany, start=1, stop=3)
 
+# Create Dataset of Interest
+data_study <- data_vehicles[, c("co2TailpipeGpm","barrels08","comb08","cylinders","displ","drive","fuelType","make","transmission","phevBlended","VClass","volume")]
 count <- nrow(data_study)
 head(data_study)
 str(data_study)
 summary(data_study)
 
-# Remove data with co2TailpipeGpm = NULL
-data_study <- data_vehicles %>% filter(!is.na(co2TailpipeGpm))
+# Remove NULL data
+data_study <- na.omit(data_study)
 count_study <- nrow(data_study)
 
-# Remove data with co2TailpipeGpm = 0
+# Remove -0- data
 data_study <- subset(data_study, co2TailpipeGpm != 0)
+data_study <- subset(data_study, cylinders != 0)
+data_study <- subset(data_study, displ != 0)
 count_study <- nrow(data_study)
 summary(data_study$co2TailpipeGpm)
+
+# Create Pearson Correlation Coefficient Matrix
+data_study_numeric <- data_study[, c("co2TailpipeGpm","barrels08","comb08","cylinders","displ","volume")]
+count_study_numeric <- nrow(data_study_numeric)
+correlation_matrix <- cor(data_study_numeric)
+print(correlation_matrix)
+ggcorrplot(correlation_matrix, lab = TRUE)
+ggcorrplot(correlation_matrix, type = "lower", lab = TRUE)
+rm(data_study_numeric)
+rm(count_study_numeric)
+
+# Remove variables aligned with CO2 emissions and volume, which is missing for some vehicle types
+data_study <- data_study[, c("co2TailpipeGpm","cylinders","displ","drive","fuelType","make","transmission","phevBlended","VClass")]
+count_study <- nrow(data_study)
+count_removedvehicles <- count_vehicles - count_study
+summary(data_study)
